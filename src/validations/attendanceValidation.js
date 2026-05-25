023c5192c61dce8c9fbfd15b44901b71470ea808
codewithhype06 <codewithhype06@gmail.com>
2026-05-25 13:56:22 +0530

    fix: error in latency

--- src/validations/attendanceValidation.js
+++ src/validations/attendanceValidation.js
@@ -4,7 +4,7 @@ const Joi = require('joi');
 const attendanceSchema = Joi.object({
     latitude: Joi.string().required(),
     longitude: Joi.string().required(),
-    selfieImage: Joi.string().required()
+   
 });
 
 module.exports = { attendanceSchema };
